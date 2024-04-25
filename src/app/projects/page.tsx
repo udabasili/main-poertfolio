27da6a52222deac1b8b5cabcf9f170edc9f82e8b
Udbasili <33573587+udabasili@users.noreply.github.com>
2024-04-24 22:35:39 -0300

    Major updates

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,9 +7,9 @@ import { Project } from '@/feature/projects';
 
 export default function Projects() {
     return (
-        <Layout>
+        <>
             <CustomPageHeader title={'Projects'} link={'projects'} path={'projects'} />
             <Project />
-        </Layout>
+        </>
     );
 }
