fde9f01cbd5841887e559edd6bc4c3b571c9926f
Udbasili <33573587+udabasili@users.noreply.github.com>
2025-02-24 12:56:23 -0400

    Updated portfolio

--- src/app/feature/projects/components/Project.tsx
+++ src/app/feature/projects/components/Project.tsx
@@ -11,8 +11,10 @@ const imagePaths = [
     'portfolio/sample-portfolio/whax7gbm8huekiqp0kz8',
     'portfolio/sample-portfolio/pjnynvdp79tqobygmzeb',
     'portfolio/sample-portfolio/gi5dk98hts8dkwymsm5m',
+    'portfolio/sample-portfolio/whax7gbm8huekiqp0kz8',
+    'portfolio/sample-portfolio/pjnynvdp79tqobygmzeb',
+    'portfolio/sample-portfolio/gi5dk98hts8dkwymsm5m',
 ];
-
 
 export const Project = () => {
     const { open, close, isOpen } = useDisclosure();
